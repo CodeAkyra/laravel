02b4bf032082e28d1469406e3a50368c6de5ef65
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2025-01-10 02:31:19 +0800

    learning basic route in laravel

---
+++ my-laravel/app/Http/Controllers/RouteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    function home()
+    {
+        return view('home');
+    }
+    function about()
+    {
+        return view('about');
+    }
+}
--- my-laravel/routes/web.php
+++ my-laravel/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RouteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('layout');
+// });
+
+Route::get('/home', [RouteController::class, 'home']);
+Route::get('/about', [RouteController::class, 'about']);
+
+// Route::get('/home', 'RouteController@home');
+// Route::get('/about', 'RouteController@about');
